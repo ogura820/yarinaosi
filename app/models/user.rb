005ac5e5190fc1870839dd184f91cf80151d5cd4
ryo ono <ryo.ono@gmail.com>
2023-06-09 11:50:40 +0900

    Add_bcrypt-ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   before_validation { email.downcase! }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 },
