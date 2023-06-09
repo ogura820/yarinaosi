ad2869f830cbb481730cea2174f185044c16efd0
ryo ono <ryo.ono@gmail.com>
2023-06-09 11:56:50 +0900

    Add_ユーザー作成画面(users/new) exit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: true
-end
+
 end
