c2cdad5de5835597f5273655a41666a382cf1b03
ryo ono <ryo.ono@gmail.com>
2023-06-11 00:54:13 +0900

    Fix_seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,12 @@
 User.create(name: "admin", email: "admin@gmail.com", 
 password: "testtest",password_confirmation: "testtest", admin: true )
+
+User.create(name: "not_admin", email: "notadmin@gmail.com", 
+password: "testtest",password_confirmation: "testtest", id: 999 )
+
+User.all.each do |user|
+  user.tasks.create!(
+    substance: 'タイトル',
+    content: 'テキストテキストテキストテキスト'
+  )
+end
