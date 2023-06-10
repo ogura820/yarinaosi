2b4728df335f9167aa40f93337b4deb24402fe3f
ryo ono <ryo.ono@gmail.com>
2023-06-10 14:57:30 +0900

    ™Add_管理者用CRUD機能

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,49 @@
+class Admin::UsersController < ApplicationController
+  def index
+    @users = User.select(:id, :name)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_user_path(@user.id), notice: "ユーザー「#{@user.id}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(users_params)
+      redirect_to users_path, notice: "ユーザー情報を編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: "ユーザーを削除しました！"
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root to: "tasks#index"
+
+  namespace  :admin do
+    resources :users
+  end
+  
   resources :tasks
   resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
