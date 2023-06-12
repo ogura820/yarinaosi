8f03d2b31d6c3a2f532547edc4502911d29b6eeb
ryo ono <ryo.ono@gmail.com>
2023-06-12 11:31:45 +0900

    Add_Labelに１０個seed作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,13 @@ User.all.each do |user|
   )
 end
 
+Label.create(phrase:"仕事")
+Label.create(phrase:"家庭")
+Label.create(phrase:"個人")
+Label.create(phrase:"趣味")
+Label.create(phrase:"必須")
+Label.create(phrase:"可能ならば")
+Label.create(phrase:"学校")
+Label.create(phrase:"今日中")
+Label.create(phrase:"今週中")
+Label.create(phrase:"今月中")
