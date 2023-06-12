5425728df4c09590e7799ef0e8e3fe32dc4f1ec0
ryo ono <ryo.ono@gmail.com>
2023-06-12 11:34:08 +0900

    Add_Labellingsテーブル（LabelとTaskの中間テーブル作成）

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_12_021904) do
+ActiveRecord::Schema.define(version: 2023_06_12_023224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "labellings", force: :cascade do |t|
+    t.bigint "task_id", null: false
+    t.bigint "label_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["label_id"], name: "index_labellings_on_label_id"
+    t.index ["task_id"], name: "index_labellings_on_task_id"
+  end
 
   create_table "labels", force: :cascade do |t|
     t.string "phrase"
@@ -44,5 +53,7 @@ ActiveRecord::Schema.define(version: 2023_06_12_021904) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "labellings", "labels"
+  add_foreign_key "labellings", "tasks"
   add_foreign_key "tasks", "users"
 end
