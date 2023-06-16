c2813772f3bda5edf4d4ec3cf15fe48266ce7cd0
ryo ono <ryo.ono@gmail.com>
2023-06-16 22:56:39 +0900

    fix_deployproduction

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '35.75.67.103/', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/onomuku/.ssh/id_rs'
+server '35.75.67.103', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/onomuku/.ssh/id_rsa'
